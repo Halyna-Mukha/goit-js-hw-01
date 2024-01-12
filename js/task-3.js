08383db049e4c34cfb4b4b6b8acc5ffa38d1bde6
Halyna-Mukha <msaarf555@gmail.com>
2024-01-12 10:30:57 +0100

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,13 @@
 function getElementWidth(content, padding, border) {
 
-    const Npx = (parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2);
-  
-    console.log(Npx);
+    content = parseFloat(content)
+    padding = parseFloat(padding)
+    border = parseFloat(border)
+    const fullWidth = content + padding * 2 + border * 2;
+   
+    return fullWidth
+    
 }
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
